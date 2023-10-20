05da668d0a064105aa4e044cde91d44c8424a013
angeldev07 <angelgaran20@gmail.com>
2023-10-19 20:06:12 -0500

    feat(security): Created an exception handling class to handle the possibles exceptions.

---
+++ JwtCourse/src/main/java/com/jwt/JwtCourse/exceptions/EmailExistException.java
@@ -0,0 +1,7 @@
+package com.jwt.JwtCourse.exceptions;
+
+public class EmailExistException extends  Exception{
+    public EmailExistException(String message) {
+        super(message);
+    }
+}
---
+++ JwtCourse/src/main/java/com/jwt/JwtCourse/exceptions/ExceptionHandling.java
@@ -0,0 +1,113 @@
+package com.jwt.JwtCourse.exceptions;
+
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.jwt.JwtCourse.http.responses.HttpResponse;
+import jakarta.persistence.NoResultException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestControllerAdvice
+public class ExceptionHandling {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static final String ACCOUNT_LOCKED = "Your account has been locked";
+    private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a %s request";
+    private static final String INTERNAL_ERROR_MSG = "An error occurred while processing the request";
+    private static final String ACCOUNT_DISABLED= "Your account has been disable.";
+    private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
+    private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    private static final String INCORRECT_CREDENTIALS  = "Username or password incorrect. Please try again";
+
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> accountDisabledException (){
+        return createHttpResponse(BAD_REQUEST, ACCOUNT_DISABLED);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<HttpResponse> badCredentialsException (){
+        return createHttpResponse(BAD_REQUEST, INCORRECT_CREDENTIALS);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<HttpResponse> accessDeniedException (){
+        return createHttpResponse(FORBIDDEN, NOT_ENOUGH_PERMISSION);
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> accountLockedException (){
+        return createHttpResponse(UNAUTHORIZED, ACCOUNT_LOCKED);
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<HttpResponse>  tokenExpiredException (TokenExpiredException exception){
+        return createHttpResponse(BAD_REQUEST, exception.getMessage().toUpperCase());
+    }
+
+    @ExceptionHandler(EmailExistException.class)
+    public ResponseEntity<HttpResponse>  emailExistsException (EmailExistException exception){
+        return createHttpResponse(BAD_REQUEST, exception.getMessage().toUpperCase());
+    }
+
+    @ExceptionHandler(EmailNotFoundException.class)
+    public ResponseEntity<HttpResponse>  emailNotFoundException (EmailNotFoundException exception){
+        return createHttpResponse(BAD_REQUEST, exception.getMessage().toUpperCase());
+    }
+
+    @ExceptionHandler(UsernameExistException.class)
+    public ResponseEntity<HttpResponse>  usernameExistsException (UsernameExistException exception){
+        return createHttpResponse(BAD_REQUEST, exception.getMessage().toUpperCase());
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<HttpResponse>  usernameNotFountException (UsernameNotFoundException exception){
+        return createHttpResponse(BAD_REQUEST, exception.getMessage().toUpperCase());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<HttpResponse> methodNotAllowedException (HttpRequestMethodNotSupportedException exception){
+        HttpMethod supportedMethod = Objects.requireNonNull(exception.getSupportedHttpMethods()).iterator().next();
+        return createHttpResponse(METHOD_NOT_ALLOWED, String.format(METHOD_IS_NOT_ALLOWED, supportedMethod));
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpResponse> internalErrorException (Exception exception){
+        LOGGER.error(exception.getMessage());
+        return createHttpResponse(INTERNAL_SERVER_ERROR, INTERNAL_ERROR_MSG);
+    }
+
+    @ExceptionHandler(NoResultException.class)
+    public ResponseEntity<HttpResponse> notFoundException (NoResultException exception){
+        LOGGER.error(exception.getMessage());
+        return createHttpResponse(NOT_FOUND, exception.getMessage());
+    }
+
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<HttpResponse> iOException (IOException exception){
+        LOGGER.error(exception.getMessage());
+        return createHttpResponse(NOT_FOUND, exception.getMessage());
+    }
+
+    private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
+        return new ResponseEntity<>(
+                new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase(), message.toUpperCase()),
+                httpStatus
+        );
+    }
+}
